f62230826f8b660b5a82dfac4a7a5c991480e5b1
BONE <kevinhhpedrozo@gmail.com>
2026-06-03 13:45:16 -0500

    Create listar.php to display registered students

---
+++ listar.php
@@ -0,0 +1,70 @@
+<?php
+
+$archivo = "data/estudiantes.json";
+
+if(!file_exists($archivo))
+{
+    die("No existen estudiantes registrados");
+}
+
+$estudiantes = json_decode(
+file_get_contents($archivo),
+true
+);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="UTF-8">
+<title>Listado</title>
+</head>
+
+<body>
+
+<h1>📋 Estudiantes Registrados</h1>
+
+<table border="1" cellpadding="10">
+
+<tr>
+<th>Nombre</th>
+<th>Documento</th>
+<th>Correo</th>
+<th>Fecha</th>
+</tr>
+
+<?php
+
+foreach($estudiantes as $e)
+{
+    echo "
+
+    <tr>
+
+    <td>{$e['nombre']}</td>
+
+    <td>{$e['documento']}</td>
+
+    <td>{$e['correo']}</td>
+
+    <td>{$e['fecha']}</td>
+
+    </tr>
+
+    ";
+}
+
+?>
+
+</table>
+
+<br>
+
+<a href="index.php">
+🏠 Volver
+</a>
+
+</body>
+</html>
