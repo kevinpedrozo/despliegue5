f36907a5ee2175f6fa1d1848cbee688919d741c2
BONE <kevinhhpedrozo@gmail.com>
2026-06-03 23:59:00 -0500

    Change file paths to use system temp directory

--- verificar.php
+++ verificar.php
@@ -1,7 +1,7 @@
 <?php
 
-$archivoEstudiantes = __DIR__ . "/estudiantes.json";
-$archivoDocumentos  = __DIR__ . "/documentos.json";
+$archivoEstudiantes = sys_get_temp_dir() . "/estudiantes.json";
+$archivoDocumentos  = sys_get_temp_dir() . "/documentos.json";
 
 if (!file_exists($archivoEstudiantes)) {
     file_put_contents($archivoEstudiantes, "[]");
